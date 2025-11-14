83c6dac6cf8f87f2bf7456d40821b4417a57cbca
Dheerissara <166388307+Dheerissara@users.noreply.github.com>
2025-11-14 23:40:17 +0700

    Update b01bd56e316b5e65@81.js

--- b01bd56e316b5e65@81.js
+++ b01bd56e316b5e65@81.js
@@ -276,7 +276,7 @@ async function _nodeImages(FileAttachment)
 function _chart(periods,periodIndex,dataByPeriod,corrThreshold,d3,corrColor,drag,nodeImages)
 {
   const width = 1000;
-  const height = 830;
+  const height = 700;
 
   const period = periods[periodIndex];
   const {nodes, links} = dataByPeriod.get(period.id);
