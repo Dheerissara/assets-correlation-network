caa886f89e51ee3aa761410f8259a1dd64f26738
Dheerissara <166388307+Dheerissara@users.noreply.github.com>
2025-11-18 02:02:45 +0700

    Update b01bd56e316b5e65@145.js

--- b01bd56e316b5e65@145.js
+++ b01bd56e316b5e65@145.js
@@ -550,12 +550,12 @@ function _chart(periods,periodIndex,dataByPeriod,corrRange,corrRangeMode,d3,corr
    .force("link",
       d3.forceLink(linksWithRef)
        .id(d => d.id)
-        .distance(d => 320 * (1 - d.abs_corr) + 100)   // เพิ่มระยะเส้น
-        .strength(d => d.abs_corr * 0.5)              // ลดแรงดึงลง
+        .distance(d => 300 * (1 - d.abs_corr) + 90)   // เพิ่มระยะเส้น
+        .strength(d => d.abs_corr * 0.6)              // ลดแรงดึงลง
     )
-    .force("charge", d3.forceManyBody().strength(-200))  // ดัน node ออก
+    .force("charge", d3.forceManyBody().strength(-180))  // ดัน node ออก
     .force("center", d3.forceCenter(width / 2, height / 2))
-    .force("collision", d3.forceCollide().radius(d => nodeSize(d.strength) + 11)); // กันชนใหญ่ขึ้น
+    .force("collision", d3.forceCollide().radius(d => nodeSize(d.strength) + 10)); // กันชนใหญ่ขึ้น
 
 
   // draw links (ของเดิม ไม่ต้องแก้)
